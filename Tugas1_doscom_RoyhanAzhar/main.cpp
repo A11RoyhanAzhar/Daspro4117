9687bfb8f1aafa23b54dc4f5f4b1909913d93275
RoyhanAzhar14629 <112872105+A11RoyhanAzhar@users.noreply.github.com>
2022-09-06 10:25:58 +0700

    Add files via upload

---
+++ Tugas1_doscom_RoyhanAzhar/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "MY NAME IS" << endl;
+    cout << "" << endl;
+    cout << "NAMA : ROYHAN AZHAR" << endl;
+    cout << "NIM : A11.2022.14629" << endl;
+    cout << "ASAL SEKOLAH : SMK TELEKOMUNIKASI TUNAS HARAPAN"<<endl;
+    return 0;
+}
